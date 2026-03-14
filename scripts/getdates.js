75f8167548115e80c064fb0b32befe2f9e50b3fd
gracelyyy <Gracely166@gmail.com>
2026-03-14 17:17:13 +0200

    Add script to display current year and last modified date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,7 @@
 <script src="scripts/getdates.js" defer></script>
+
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent =
+"Last Modified: " + document.lastModified;
